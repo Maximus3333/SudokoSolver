301f6de02caa6b527abe869d84f0e5411989b373
Max Deisadze <m.deisadze@hotmail.com>
2021-12-16 01:43:05 -0500

    finished product

--- solver.py
+++ solver.py
@@ -8,62 +8,66 @@ def solveSudoku(board) -> None:
     cells = sub_boxes_and_cell_values[1]
     columnboard = column_board(board)
     cell_coordinates = ""
-    rowcounter = 0
-    columncounter = 0
-    for cell_coordinates_and_sub_box_coordinates_key in cells:
-        cell_value = cells[cell_coordinates_and_sub_box_coordinates_key][0]
-        sub_box_coordinates = cells[cell_coordinates_and_sub_box_coordinates_key][1]
-        if cell_value == '.':
-            values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-            for values_in_sub_box in sub_box_dict[sub_box_coordinates]:
-                if values_in_sub_box in values:
-                    values.remove(values_in_sub_box)
-            row = board[int(cell_coordinates_and_sub_box_coordinates_key[0])]
-            column = columnboard[int(cell_coordinates_and_sub_box_coordinates_key[2])]
-            for row_values in row:
-                if row_values in values:
-
-    for sub_box_key in subbox_dict:
-        values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        for cell in subbox_dict[sub_box_key]:
-            if columncounter >= int(sub_box_key[2]):
-                columncounter = 0
-                rowcounter += 1
-            cell_coordinates = "" + rowcounter + "x" + "" + columncounter
-            if cell == '.':
-                if cell_coordinates not in possible_values_in_cells.keys():
-
-            possible_values_in_cells[cell_coordinates] = []
-            possible_values_in_cells[cell_coordinates] += values
-            if int(cell) in values:
-
-            if cell == '.':
-
-                if int(cell) in values:
-                    values.remove(int(cell))
-                for columns in
-
-            columncounter += 1
-
-
-
-
-        if cell_coordinates not in possible_values_in_cells.keys():
-            values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-            columncounter = 0
-            for cell in row:
-                if cell == '.':
-
-                    if int(cell) in values:
-                        values.remove(int(cell))
-                    for columns in
-
-        cell_coordinates = "" + rowcounter + "x" + "" + columncounter
-        possible_values_in_cells[cell_coordinates] = []
-        possible_values_in_cells[cell_coordinates] += values
-
-        columncounter += 1
-    rowcounter += 1
+    recurse = 0
+    first_inst = 1
+    while recurse == 1 or first_inst == 1:
+        first_inst = 0
+        recurse = 0
+        for cell_coordinates_and_sub_box_coordinates_key in cells:
+            cell_value = cells[cell_coordinates_and_sub_box_coordinates_key][0]
+            sub_box_coordinates = cells[cell_coordinates_and_sub_box_coordinates_key][1]
+            if cell_value == '.':
+                values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            elif type(cell_value) == list:
+                values = cell_value
+                # print(values, cell_coordinates_and_sub_box_coordinates_key)
+            if cell_value == '.' or type(cell_value) == list:
+                recurse = 1
+                # print(cell_value, cell_coordinates_and_sub_box_coordinates_key)
+                # print(sub_box_dict[sub_box_coordinates])
+                for values_in_sub_box in sub_box_dict[sub_box_coordinates]:
+                    # print(values_in_sub_box[0])
+                    value = values_in_sub_box[0]
+                    # val_pos = values_in_sub_box[1]
+                    if value != ".":
+                        value = int(value)
+                        if value in values:
+                            # print(value)
+                            values.remove(value)
+                            # print(values, cell_coordinates_and_sub_box_coordinates_key)
+                row = board[int(cell_coordinates_and_sub_box_coordinates_key[0])]
+                column = columnboard[int(cell_coordinates_and_sub_box_coordinates_key[2])]
+                for row_values in row:
+                    if row_values != ".":
+                        row_values = int(row_values)
+                        if row_values in values:
+                            values.remove(row_values)
+                            # print(row_values)
+                for column_values in column:
+                    # print(column_values)
+                    if column_values != ".":
+                        column_values = int(column_values)
+                        if column_values in values:
+                            values.remove(column_values)
+                            # print(column_values)
+                if len(values) == 1:
+                    cells[cell_coordinates_and_sub_box_coordinates_key][0] = values[0]
+                    # print(board[int(cell_coordinates_and_sub_box_coordinates_key[0])][int(cell_coordinates_and_sub_box_coordinates_key[2])])
+                    # print(row[int(cell_coordinates_and_sub_box_coordinates_key[0])][int(cell_coordinates_and_sub_box_coordinates_key[2])])
+                    board[int(cell_coordinates_and_sub_box_coordinates_key[0])][int(cell_coordinates_and_sub_box_coordinates_key[2])] = str(values[0])
+                    columnboard[int(cell_coordinates_and_sub_box_coordinates_key[2])][int(cell_coordinates_and_sub_box_coordinates_key[0])] = str(values[0])
+                    count = 0
+                    for values_in_sub_box in sub_box_dict[sub_box_coordinates]:
+                        val_pos = values_in_sub_box[1]
+                        # print(values_in_sub_box)
+                        if val_pos == cell_coordinates_and_sub_box_coordinates_key:
+                        #     # print(sub_box_dict[sub_box_coordinates][count])
+                            sub_box_dict[sub_box_coordinates][count][0] = str(values[0])
+                            # print(sub_box_dict[sub_box_coordinates][count])
+                        #     # print(sub_box_dict[sub_box_coordinates][count])
+                        count+=1
+                else:
+                    cells[cell_coordinates_and_sub_box_coordinates_key][0] = values
 
 def subboxes(rowboard):
     subbox_dict = {}
@@ -73,8 +77,9 @@ def subboxes(rowboard):
     for row in rowboard:
         columncounter = 0
         for cell in row:
+            cell_coordinates = "" + str(rowcounter) + "x" + "" + str(columncounter)
             if columncounter % 3 == 0 and rowcounter % 3 == 0:
-                subbox = [cell]
+                subbox = [[cell, cell_coordinates]]
                 key = ""+ str(rowcounter+3) + "x" + str(columncounter+3)
                 subbox_dict[key] = subbox
                 currentkey = key
@@ -83,13 +88,12 @@ def subboxes(rowboard):
                     if int(key[0])>rowcounter and rowcounter >= int(key[0])-3 and int(key[2])>columncounter and int(key[2])-3<=columncounter:
                         currentkey = key
                 subbox = subbox_dict[currentkey]
-                subbox.append(cell)
-            cell_coordinates = "" + str(rowcounter) + "x" + "" + str(columncounter)
+                subbox.append([cell, cell_coordinates])
             # print(cell_coordinates)
             cell_dict[cell_coordinates] = [cell, currentkey]
             columncounter+=1
         rowcounter+=1
-    # print(cell_dict)
+    # print(subbox_dict)
     return subbox_dict, cell_dict
 
 def column_board(rowboard):
@@ -106,9 +110,10 @@ def column_board(rowboard):
                 column.append(cell)
             columncounter+=1
         rowcounter+=1
-
     return new_list
 
 board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
 # column_board(board)
-print(subboxes(board))
+# print(subboxes(board)[0])
+solveSudoku(board)
+# subboxes(board)
